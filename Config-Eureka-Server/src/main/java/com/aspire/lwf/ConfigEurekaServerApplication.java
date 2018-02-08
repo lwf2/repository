e37dc4dfbf78108f326ed81a11fda8737359df73
liuweifeng <1366841964@qq.com>
2018-02-08 10:39:33 +0800

    你哈佛

--- Config-Eureka-Server/src/main/java/com/aspire/lwf/ConfigEurekaServerApplication.java
+++ Config-Eureka-Server/src/main/java/com/aspire/lwf/ConfigEurekaServerApplication.java
@@ -2,9 +2,9 @@ package com.aspire.lwf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigEurekaServerApplication {
 
